dd960b7a6b9f9777ee279233ede57005803de0fa
Jonathan Gruber <jonathan.gruber@imperva.com>
2019-12-28 00:08:30 +0200

    updated

--- src/dashboard_scripts.js
+++ src/dashboard_scripts.js
@@ -1,14 +1,123 @@
-const request = require('request')
 const fs = require('fs')
+const axios = require('axios')
+const querystring = require('querystring');
 
 
-const sites_list = ({ api_id, api_key, account_id } = {}, callback) => {
+const sites_list = ({ api_id, api_key, account_id, period } = {}, callback) => {
     console.log("received JSON")
-    console.log({ api_id, api_key, account_id })
+    console.log({ api_id, api_key, account_id, period })
+
+
+    const post_data = {
+        api_id: api_id,
+        api_key: api_key,
+        account_id: account_id
+        //       'page_size': 100
+    }
+
+    const post_stats = {
+        api_id: api_id,
+        api_key: api_key,
+        account_id: account_id,
+        stats: 'visits_timeseries, hits_timeseries, bandwidth_timeseries, requests_geo_dist_summary, visits_dist_summary, caching, caching, caching_timeseries, threats, incap_rules, incap_rules_timeseries',
+        time_range: period
+    }
+
+
+// PROMISE FOR ALL CALLS
+axios.all([
+
+    // SITE LIST JSON
+    axios.post('https://my.imperva.com/api/prov/v1/sites/list', querystring.stringify(post_data)),
+    /*
+    .then((response) => {
+        console.log("RETRIEVE THE FIRST SITE LIST")
+        console.log(response.data)
+        if (response.data.res_message != "OK") {
+            fs.writeFileSync('public/export_sites.json', response.data)
+        } else {
+            //  var sites_array_a = JSON.parse(response.body)
+            var sites_array = response.data.sites
+            console.log("export ARRAY")
+            console.log(sites_array)
+            console.log(sites_array.length)
+            fs.writeFileSync('public/export_sites.json', JSON.stringify(sites_array))
+        }
+    }, (error) => {
+        console.log(error);
+    }),
+*/
+    // POST SUBACCOUNTS
+    axios.post('https://my.imperva.com/api/prov/v1/accounts/subscription', querystring.stringify(post_data)),
+/*        .then((response) => {
+            console.log("POST SUBSCRIPTION")
+            console.log(response.data)
+            if (response.data.res_message != "OK") {
+                fs.writeFileSync('public/export_account_subscriptions.json', JSON.stringify(response.data))
+
+            } else {
+                console.log("WEEENNTT OKKK")
+                fs.writeFileSync('public/export_account_subscriptions.json', JSON.stringify(response.data))
+            }
+        }, (error) => {
+            console.log(error);
+        }),
+*/
+    // POST SUBACCOUNTS
+    axios.post('https://my.imperva.com/api/prov/v1/accounts/listSubAccounts', querystring.stringify(post_data)),
+    /*    .then((response) => {
+            console.log("POST SUBACCOUNT")
+            console.log(response.data)
+            fs.writeFileSync('public/export_subaccounts.json', JSON.stringify(response.data))
+        }, (error) => {
+            console.log(error);
+        }),
+*/
+
+
+    // POST STATS
+    axios.post('https://my.imperva.com/api/stats/v1', querystring.stringify(post_stats))
+     /*   .then((response) => {
+            console.log("POST STATS")
+            console.log(response.data)
+            fs.writeFileSync('public/export_account_stats.json', JSON.stringify(response.data))
+        }, (error) => {
+            console.log(error);
+        })   
+    */
+    ])
+.then(axios.spread((response_sites, response_sub1, r3, r4) => {
+
+    console.log("PROMISE #1")
+    console.log(r4)
+
+   // fs.writeFileSync('public/export_sites.json', JSON.stringify(r4.data))
+
+  //this will be executed only when all requests are complete
+  callback({ res_message: "OK" })
+
+}))  
+.catch((error) => {
+    console.log("THIS IS AN AXIOS ERROR")
+    console.log(error);
+   });
+
+
+}
+
+
+
+
+module.exports = {
+    sites_list
+}
+
+
+
 
 
 /*
-    // FUNCTION TO FETCH MORE PAGES IF MORE THAN 100 SITES 
+    // FUNCTION TO FETCH MORE PAGES IF MORE THAN 100 SITES
     function requestExtra(pageNb) {
         console.log("SOMEONE IS REQUESTING EXTRA")
         const post_extra = {
@@ -52,37 +161,7 @@ const sites_list = ({ api_id, api_key, account_id } = {}, callback) => {
     }
 
 */
-
-    const post_data = {
-        api_id: api_id,
-        api_key: api_key,
-        account_id: account_id,
-        'page_size': 100
-    }
-    const post_list = {
-        method: 'POST',
-        url: 'https://my.imperva.com/api/prov/v1/sites/list',
-        formData: post_data,
-        headers: {
-            'cache-control': 'no-cache'
-        }
-    }
-    request(post_list, (error, response) => {
-        if (error) {
-            console.log("error " + error)
-            // NEED TO ADD SMTHG heRE TO HANDLE WHEN UNDEFINED
-        } else {
-            const response_a = JSON.parse(response.body)
-            if (response_a.res_message != "OK") {
-                fs.writeFileSync('public/export_sites.json', response.body)
-
-            } else {
-                var sites_array_a = JSON.parse(response.body)
-                var sites_array = sites_array_a.sites
-                console.log("export ARRAY")
-                console.log(sites_array)
-                console.log(sites_array.length)
-
+/*
                 // TO CREATE THE LOOP IF MORE THAN 100 SITES
 
                 /*
@@ -106,87 +185,10 @@ const sites_list = ({ api_id, api_key, account_id } = {}, callback) => {
                     }
                 }
                 */
-                fs.writeFileSync('public/export_sites.json', JSON.stringify(sites_array))
 
-            }
-        }
-    })
-
-
-
-    // POST SUBSCRIPTION
-    const post_subs = {
-        method: 'POST',
-        url: 'https://my.imperva.com/api/prov/v1/accounts/subscription',
-        formData: post_data
-    }
-    request(post_subs, (error, response) => {
-        if (error) {
-            console.log("error " + error)
-        } else {
-            console.log("post data")
-            console.log(post_subs)
-            console.log("subscription call")
-            console.log(response.body)
-            fs.writeFileSync('public/export_account_subscriptions.json', response.body)
-            //       callback(undefined, response.body)
-        }
-    })
-
-    // POST SUBACCOUNTS
-    const post_subaccount = {
-        method: 'POST',
-        url: 'https://my.imperva.com/api/prov/v1/accounts/listSubAccounts',
-        formData: post_data
-    }
-    request(post_subaccount, (error, response) => {
-        if (error) {
-            console.log("error " + error)
-        } else {
-            console.log("post data")
-            console.log(post_subaccount)
-            console.log("subaccounts call")
-            console.log(response.body)
-            fs.writeFileSync('public/export_subaccounts.json', response.body)
-            //       callback(undefined, response.body)
-        }
-    })
-
-    // POST STATS
-    const post_data_stats = {
-        method: 'POST',
-        url: 'https://my.imperva.com/api/stats/v1',
-        formData: {
-            api_id: api_id,
-            api_key: api_key,
-            account_id: account_id,
-            stats: 'visits_timeseries, hits_timeseries, bandwidth_timeseries, requests_geo_dist_summary, visits_dist_summary, caching, caching, caching_timeseries, threats, incap_rules, incap_rules_timeseries',
-            time_range: 'last_30_days',
-            page_size: 100,
-            page_num: 0
-        }
-    }
-    request(post_data_stats, (error, response) => {
-        if (error) {
-            console.log("error " + error)
-        } else {
-            console.log("post data_stats")
-            console.log(post_subaccount)
-            console.log("data_stats call")
-            console.log(response.body)
-            fs.writeFileSync('public/export_account_stats.json', response.body)
-            //       callback(undefined, response.body)
-        }
-    })
-
-    callback({ res_message: "OK" })
-
+                /*
+fs.writeFileSync('public/export_sites.json', JSON.stringify(sites_array))
 }
-
-
-module.exports = {
-    sites_list
 }
-
-
-
+}) 
+*/
